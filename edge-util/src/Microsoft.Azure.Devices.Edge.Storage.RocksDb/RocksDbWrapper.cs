e682d816a84ce7611a67b95263ee3df473d65f7f
Shantanu Singh <shsi@microsoft.com>
2019-09-03 18:41:26 -0700

    Working changes

--- edge-util/src/Microsoft.Azure.Devices.Edge.Storage.RocksDb/RocksDbWrapper.cs
+++ edge-util/src/Microsoft.Azure.Devices.Edge.Storage.RocksDb/RocksDbWrapper.cs
@@ -3,7 +3,6 @@ namespace Microsoft.Azure.Devices.Edge.Storage.RocksDb
 {
     using System;
     using System.Collections.Generic;
-    using System.Diagnostics;
     using System.IO;
     using System.Linq;
     using Microsoft.Azure.Devices.Edge.Util;
